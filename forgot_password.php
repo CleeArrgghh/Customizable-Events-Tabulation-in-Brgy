827eb17dc5e98a34d422deb8ac04d77a64114c33
CleeArr <129164257+CleeArrgghh@users.noreply.github.com>
2024-02-12 09:21:11 +0800

    Update forgot_password.php
    removed email

--- forgot_password.php
+++ forgot_password.php
@@ -93,7 +93,7 @@ use PHPMailer\PHPMailer\Exception;
                     $mail->isSMTP();                                      
                     $mail->Host       = 'smtp.gmail.com';                 
                     $mail->SMTPAuth   = true;                             
-                    $mail->Username   = 'cleearr.canillas@lsu.edu.ph';           
+                    $mail->Username   = '';           
                     $mail->Password   = '';                  
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                            
                     $mail->Port       = 587;                              
